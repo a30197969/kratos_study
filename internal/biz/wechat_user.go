3b1176ed78fa43d7d81b691dbbf3506c8a54553a
peng.liang <peng.liang@fengniao.com>
2022-01-14 18:26:33 +0800

    时间格式处理

--- internal/biz/wechat_user.go
+++ internal/biz/wechat_user.go
@@ -16,34 +16,34 @@ type WechatUser struct {
 	// Openid 用户的唯一标识
 	Openid string `gorm:"not null;type:varchar(50);index"`
 	// WxID 微信号
-	WxID          string `gorm:"type:varchar(50)"`
-	Level         uint8  `gorm:"not null;default:1"`
-	Phone         string `gorm:"type:varchar(20);index"`
-	PurePhone     string `gorm:"type:varchar(20)"`
-	CountryCode   string `gorm:"type:varchar(10)"`
-	Nickname      string `gorm:"not null;type:varchar(50)"`
-	Sex           uint8  `gorm:"not null;default:0"`
-	Language      string `gorm:"not null;type:varchar(50)"`
-	ProvinceCode  string `gorm:"type:varchar(10)"`
-	Province      string `gorm:"type:varchar(50)"`
-	CityCode      string `gorm:"type:varchar(10)"`
-	City          string `gorm:"type:varchar(50)"`
-	DistrictCode  string `gorm:"type:varchar(10)"`
-	District      string `gorm:"type:varchar(50)"`
-	Country       string `gorm:"type:varchar(50)"`
-	Headimgurl    string `gorm:"type:varchar(200)"`
-	Remark        string `gorm:"type:varchar(200)"`
-	Groupid       uint   `gorm:"not null;default:0"`
-	Privilege     string `gorm:"type:varchar(250)"`
-	Unionid       string `gorm:"type:varchar(50);index"`
-	Subscribe     uint   `gorm:"not null;default:0"`
-	SubscribeTime uint   `gorm:"not null;default:0"`
-	Dateline      string
-	IsDel         uint8  `gorm:"not null;default:0"`
-	Appid         string `gorm:"not null;type:varchar(50)"`
-	PicID         uint   `gorm:"not null;default:0"`
-	TagidList     string `gorm:"type:varchar(250)"`
-	UpdateDate    string
+	WxID          string    `gorm:"type:varchar(50)"`
+	Level         uint8     `gorm:"not null;default:1"`
+	Phone         string    `gorm:"type:varchar(20);index"`
+	PurePhone     string    `gorm:"type:varchar(20)"`
+	CountryCode   string    `gorm:"type:varchar(10)"`
+	Nickname      string    `gorm:"not null;type:varchar(50)"`
+	Sex           uint8     `gorm:"not null;default:0"`
+	Language      string    `gorm:"not null;type:varchar(50)"`
+	ProvinceCode  string    `gorm:"type:varchar(10)"`
+	Province      string    `gorm:"type:varchar(50)"`
+	CityCode      string    `gorm:"type:varchar(10)"`
+	City          string    `gorm:"type:varchar(50)"`
+	DistrictCode  string    `gorm:"type:varchar(10)"`
+	District      string    `gorm:"type:varchar(50)"`
+	Country       string    `gorm:"type:varchar(50)"`
+	Headimgurl    string    `gorm:"type:varchar(200)"`
+	Remark        string    `gorm:"type:varchar(200)"`
+	Groupid       uint      `gorm:"not null;default:0"`
+	Privilege     string    `gorm:"type:varchar(250)"`
+	Unionid       string    `gorm:"type:varchar(50);index"`
+	Subscribe     uint      `gorm:"not null;default:0"`
+	SubscribeTime uint      `gorm:"not null;default:0"`
+	Dateline      time.Time `gorm:"autoCreateTime;type:datetime"`
+	IsDel         uint8     `gorm:"not null;default:0"`
+	Appid         string    `gorm:"not null;type:varchar(50)"`
+	PicID         uint      `gorm:"not null;default:0"`
+	TagidList     string    `gorm:"type:varchar(250)"`
+	UpdateDate    time.Time `gorm:"autoUpdateTime;type:datetime"`
 }
 
 // WechatUserRepo 用户数据模型 在数据库上的基础操作封装为接口
@@ -68,14 +68,13 @@ func NewWechatUserUsecase(repo WechatUserRepo, logger log.Logger) *WechatUserUse
 	}
 }
 func (uc *WechatUserUsecase) CreateUser(ctx context.Context, openid string, nickname string, unionid string, appid string) (uint64, error) {
-	t := time.Now()
-	dateline := t.Format("2006-01-02 15:04:05")
+	//t := time.Now()
+	//dateline := t.Format("2006-01-02 15:04:05")
 	wechatUserModel := &WechatUser{
 		Openid:   openid,
 		Nickname: nickname,
 		Unionid:  unionid,
 		Appid:    appid,
-		Dateline: dateline,
 	}
 	return uc.repo.CreateUser(ctx, wechatUserModel)
 }
